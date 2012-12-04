db3a2fea154da2fb1e5b28cb35cde5972721cb71
Ivan Efremov <st8998@gmail.com>
2012-12-04 10:21:36 +0400

    multy column search test

--- spec/search_scope_spec.rb
+++ spec/search_scope_spec.rb
@@ -40,6 +40,43 @@ describe 'search scope' do
     end
 
   end
+
+  context 'multiple column' do
+    before do
+      Fruit.send(:search_scope_for, :name, :description, {as: :ftx, wildcard: :last, language: 'english'})
+
+      create(:fruit, name: 'apple', description: 'apple is not an orange')
+      create(:fruit, name: 'orange', description: 'orange is not a pineapple')
+      create(:fruit, name: 'pineapple') # null description
+    end
+
+    let(:ar_apple) { Fruit.where(name: 'apple') }
+    let(:ar_orange) { Fruit.where(name: 'orange') }
+    let(:ar_pineapple) { Fruit.where(name: 'pineapple') }
+    let(:ar_apple_orange) { Fruit.where(name: ['apple', 'orange']) }
+    let(:ar_orange_pineapple) { Fruit.where(name: ['orange', 'pineapple']) }
+
+    it 'should find apple in fruits' do
+      Fruit.ftx('apples').should match_array(ar_apple)
+      Fruit.ftx('apple').should match_array(ar_apple)
+      Fruit.ftx('appl').should match_array(ar_apple)
+      Fruit.ftx('app').should match_array(ar_apple)
+      Fruit.ftx('ap').should match_array(ar_apple)
+    end
+
+    it 'should find orange in apple and orange' do
+      Fruit.ftx('orange').should match_array(ar_apple_orange)
+    end
+
+    it 'should ignore null columns' do
+      Fruit.ftx('pineapple').should match_array(ar_orange_pineapple)
+    end
+
+    it 'should match with & against all columns' do
+      Fruit.ftx('apple orange').should match_array(ar_apple)
+    end
+
+  end
 end
 
 
