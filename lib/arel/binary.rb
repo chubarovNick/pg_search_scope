597ee8566dd3c2bb52bb457c02159c063508f158
Nick Chubarov <nick.chubarov@gmail.com>
2014-10-31 16:40:10 +0400

    Arel class renaming

--- lib/arel/binary.rb
+++ lib/arel/binary.rb
@@ -7,7 +7,7 @@ end
 
 module Arel
   module Visitors
-    class ToSql < Arel::Visitors::Visitor
+    class ToSql < Arel::Visitors::Reduce
       def visit_Arel_Nodes_Convert o
         "(#{visit o.left})::#{o.right}"
       end
